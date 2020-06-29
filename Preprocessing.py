256a08305f0225f78ffab63ff4d0e7d5ff2ec6a8
zxinyuan <zxy315@bu.edu>
2020-06-29 09:45:47 -0400

    add Preprocessing.py

---
+++ Preprocessing.py
@@ -0,0 +1,27 @@
+
+"""
+    Preprocessing.py
+    - function independent section
+      1. generate random lambdas
+      2. sacrifice
+      3. random linear combination
+    - function dependent section
+      1. assign lambdas to wires
+      2. generate beaver triples
+"""
+
+import os
+
+"""
+    function independent section
+"""
+
+# input: /
+# output: bytes
+# generate cryptographically secure randomness lambda
+def getLambda():
+    pass
+
+"""
+    function dependent section
+"""
