8c5cd6bfea1f4326262fe949b21bcc6a463a9f6b
all-seeing-eye <60790747+all-seeing-eye@users.noreply.github.com>
2020-07-01 03:53:42 -0400

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,3 +1,4 @@
 """
 Here we should generate many random λ with triples. Here we should also be able to verify the beaver triples. And also use fiat-shmamir.py for a source of randomness
+Moreover, we will also add the functionalities from sarah's previous comments on which parts belong to which py files. (in this case, from slides, in the circuit.py, we will apply some of the original outlined functionalities of circuit.py into this file)
 """
