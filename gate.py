1919e760e4698fe52359df26414ac05e7c83a60b
zilyang <zilingyang@DESKTOP-8FVRHLF.localdomain>
2020-07-01 11:58:50 -0400

    commit

--- gate.py
+++ gate.py
@@ -1,3 +1,21 @@
 """
 a construction of which gates wer will be using. This file is purely for writing functions in the form of logic gates. no data will be stored here)
 """
+
+class Add:
+	def __init__(input1, input2):
+		pass
+		"initiates input1 and input2 and output values"
+	def add_operation(x, y):
+		pass
+		"performs addition and assigns to output value"
+
+class Mult:
+	def __init__(input1, input2, triple1, triple2, triple3):
+		pass
+		"initiates inputs, triples, and output values"
+	def mult_operation(x, y, a, b, c):
+		pass
+		"performs multiplication and assigns to output value"
+"
+
