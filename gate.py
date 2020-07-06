196657a1899e21cab51a1b0e33b5567fc474cafe
zilyang <zilingyang@DESKTOP-8FVRHLF.localdomain>
2020-07-05 22:17:46 -0400

    change

--- gate.py
+++ gate.py
@@ -2,20 +2,18 @@
 a construction of which gates wer will be using. This file is purely for writing functions in the form of logic gates. no data will be stored here)
 """
 
-class Add:
-	def __init__(input1, input2):
+class gate:
+	#input: 2 inputs, 3 triples/ 0's
+	def __init__(self, input1, input2, triple1, triple2, triple3):	
+		self.x = input1
+		self.y = input2
+		self.a = triple1
+		self.b = triple2
+		self.c = triple3
+		self.z = 0
+	#Assigns z = a + b
+	def add(self):
 		pass
-		"initiates input1 and input2 and output values"
-	def add_operation(x, y):
+	#Assigns z = a*b
+	def mult(self):
 		pass
-		"performs addition and assigns to output value"
-
-class Mult:
-	def __init__(input1, input2, triple1, triple2, triple3):
-		pass
-		"initiates inputs, triples, and output values"
-	def mult_operation(x, y, a, b, c):
-		pass
-		"performs multiplication and assigns to output value"
-"
-
