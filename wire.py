a4dffc0d4737ee57940440dc4591e36213aa0afa
all-seeing-eye <60790747+all-seeing-eye@users.noreply.github.com>
2020-07-09 23:13:11 -0400

    Update wire.py

--- wire.py
+++ wire.py
@@ -1,1 +1,12 @@
 """refer to slide 5. This file will be used to handle data from wires"""
+
+database = {}
+
+
+def acceess_wire(index):
+    return database[index]
+
+
+def insert_wire(index, value):
+    database[index] = value
+    return 1
