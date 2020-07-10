e18164fb6dfc01c51549a43b0ea0828554f279e7
zilyang <zilingyang@DESKTOP-8FVRHLF.localdomain>
2020-07-10 12:26:34 -0400

    completed

--- gate.py
+++ gate.py
@@ -1,19 +1,23 @@
 """
 a construction of which gates wer will be using. This file is purely for writing functions in the form of logic gates. no data will be stored here)
 """
-
+import Value
+import wire
 class gate:
 	#input: 2 inputs, 3 triples/ 0's
-	def __init__(self, input1, input2, triple1, triple2, triple3):	
+	def __init__(self, input1, input2, output, triple1, triple2, triple3):	
 		self.x = input1
 		self.y = input2
 		self.a = triple1
 		self.b = triple2
 		self.c = triple3
-		self.z = 0
-	#Assigns z = a + b
+		self.z = output
+	#Assigns z = x + y
 	def add(self):
-		pass
-	#Assigns z = a*b
+		z = Value.wrapAdd(wire.acceess_wire(self.x), wire.acceess_wire( self.y))
+		wire.insert_wire(self.z, z)
+	
+	#Assigns z = x*y
 	def mult(self):
-		pass
+		z = Value.wrapMul(wire.acceess_wire(self.x), wire.acceess_wire(self.y))
+		wire.insert_wire(self.z, z) 
