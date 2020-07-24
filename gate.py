bbeaada9ad5cfa9f50e85cbb6e85a178c8e9fb47
zilyang <zilingyang@DESKTOP-8FVRHLF.localdomain>
2020-07-23 21:07:16 -0400

    finish code

--- gate.py
+++ gate.py
@@ -1,53 +1,91 @@
 """
 a construction of which gates wer will be using. This file is purely for writing functions in the form of logic gates. no data will be stored here)
 """
-#import Value
-#import wire
-import circuit
+
+from Value import Value
+
+
+'''
+# seed the pseudorandom number generator
+from random import seed
+from random import random
+# seed random number generator
+seed(1)
+
+# Generate epsilons for testing
+epsilon_1 = []*n_parties
+epsilon_2 = []*n_parties
+for i in range(n_parties):
+    epsilon_1[i] = random()
+    epsilon_2[i] = random()
+'''
+
+
 class gate:
-	#input: 2 inputs, 3 triples/ 0's
-	def __init__(self, input1, input2, output,*, triple1=None, triple2 = None, triple3 = None, operation = None):	
-		self.x = input1
-		self.y = input2
-		self.z = output
-		if operation == 'AND' or 'MUL':
-			self.a = triple1
-			self.b = triple2
-			self.c = triple3
-		if operation == 'XOR' or 'ADD':
-			self.a = None
-			self.b = None
-			self.c = None
+    # input: 2 inputs, 3 triples/ 0's
+    def __init__(self, input1, input2, output, *, triple1=Value(), triple2=Value(), triple3=Value(), operation=None):
+        self.operation = operation
+        self.x = input1
+        self.y = input2
+        self.z = output
+        if operation == 'AND' or 'MUL':
+            self.a = triple1
+            self.b = triple2
+            self.c = triple3
+        if operation == 'XOR' or 'ADD':
+            self.a = None
+            self.b = None
+            self.c = None
 
-	#Assigns v values z = x + y for each party
-	#Assign e value on output wire
-	def add(self):
-		for i in range(circuit.n_parties):
-			x_v = Value.Value(wire.acceess_wire(self.x, v, i)
-			y_v = wire.acceess_wire(self.y, v, i)
-			z_v = x_v.add(y_v)
-			wire.insert_wire(self.z, z,v,i)
-		z_e = x_e + y_e
-		wire.insert_wire(self.z, z_e) 
 
-	#Assigns v values  z = x*y for each party
-	#assign e value on output wire
-	#return e share for broadcast
-	def mult(self):
-		e_broadcast = []*circuit.n_parties
-		e = 0
-		for i in range(circuit.n_parties):
-			x_e = Value.Value(wire.acceess_wire(self.x, e))
-			y_e = Value.Value(wire.acceess_wire(self.y, e))
-			xy_e = Value.Value(x_e*y_e)
-			ay_e = Value.Value(y_e.mul(self.a[i]))
-			bx_e = Value.Value(x_e.mul(self.b[i]))
-			c = Value.Value(self.c[i])
-			z = xy_e - ay_e - bx_e + c
-			wire.insert_wire(self.z, z,v,i)
-			e_broadcast[i] = z + wire.acceess_wire(self.z, lambda, i)
-			e = e + e_broadcast[i]
-		wire.insert_wire(self,z, e)
-		return e_broadcast
-			
+'''
+    # Assigns v values z = x + y for each party
+    # Assign e value on output wire
+    def add(self):
+        z_v_arr = []*n_parties
+        # calculate z_v
+        for i in range(n_parties):
+            x_v = wire.v(self.x)[i]
+            y_v = wire.v(self.y)[i]
+            z_v = x_v + y_v
+            z_v_arr[i] = z_v
+        # set z_v
+        wire.set_v(self.z, z_v_arr)
+        # calculate z_e
+        x_e = wire.e(self.x)
+        y_e = wire.e(self.y)
+        z_e = x_e + y_e
+        # set z_e
+        wire.set_e(self.z, z_e)
 
+    # Assigns v values  z = x*y for each party
+    # assign e value on output wire
+    # return e share for broadcast
+    def mult(self):
+        #alpha_broadcast = []*circuit.n_parties
+        z_v_arr = []*n_parties
+        for i in range(n_parties):
+            # calculate z_v
+            x_e = wire.e(self.x)
+            y_e = wire.e(self.y)
+            z_v = x_e*y_e - (a[i] * y_e) - (b[i] * x_e) + c[i]
+            z_v_arr[i] = z_v
+            # calculate alpha share
+            y_lam = wire.lambda_val(self.y)[i]
+            y_lamh = wire.lambda_hat(self.y)[i]
+            #alpha_broadcast[i] = epsilon_1[i]*y_lam + (epsilon_2[i]*y_lamh)
+
+        # set z_v
+        wire.set_v(self.z, z_v_arr)
+
+        # calculate and set z_e
+        z_e = sum(wire.v(self.z).value) + sum(wire.lambda_val(self.z).value)
+        wire.set_e(self.z, Value.value(z_e))
+
+        # calculate and set z_eh
+        z_eh = sum(wire.lambda_val(self.x).value) * sum(wire.lambda_hat(self.y).value) + \
+            sum(wire.lambda_hat(self.z).value)
+        wire.set_eh(self.z, z_eh)
+
+        #return alpha_broadcast
+'''
