99eafd909d9af0f6bc17008707a43b7197108713
zilyang <zilingyang@DESKTOP-8FVRHLF.localdomain>
2020-07-23 21:07:36 -0400

    finish code

--- circuit.py
+++ circuit.py
@@ -1,96 +1,74 @@
 """see slide 6. especially for the comment from sarah for an improvement"""
 
-import sys
-import gate
-#import wire
 
-#input: external file 
-#output:in array of dictionaries. 
-#function:: parse external file
-def parse(argv, n_gate):
-	l = [None]*n_gate
-	next(argv)
-	i = 0
-	for line in argv:
-		n = line.strip('\n').split(' ')	
-		input1, input2, output, operation = int(n[2]), int(n[3]), int(n[4]), n[5]
-		g = gate.gate(input1, input2, output, operation = operation)
-		l[i] = g
-		i = i + 1
-		if i == n_gate:
-			break
-	return l	
-	
-	"""
+from Value import Value
+
+
+
+# input: external file
+# output:in array of dictionaries.
+# function:: parse external file
+
+
+def parse(argv, n_gate, gate):
+    l = [None]*n_gate
+    next(argv)
+    i = 0
+    for line in argv:
+        n = line.strip('\n').split(' ')
+        input1, input2, output, operation = int(
+            n[2]), int(n[3]), int(n[4]), n[5]
+        g = gate(input1, input2, output, operation=operation)
+        l[i] = g
+        i = i + 1
+        if i == n_gate:
+            break
+    return l
+
+    """
 	index of array corresponds to topological order of circuit
-	"""
+    """
 
-#input: array of circuit classs
+
+
+#input: array of circuit classs, wire object
 #output: array of e broadcast 
 #Write v shares and e to output value
-def compute_output(circuit):
-	broadcast = []
-	for i in range(n_gate):
-		c = circuit[i]
-		if c.operation == 'MUL' or 'AND':
-			broadcast.append(c.mul())
-		if c.operation == 'MUL' or 'XOR':
-			c.add()
-	return broadcast
-		
+def compute_output(circuit, epsilon_1, epsilon_2, wire):
+    alpha_broadcast = []
+    for i in range(n_gate):
+	c = circuit[i]
+	if c.operation == 'MUL' or c.operaton == 'AND':
+            c.mul()
+	    # calculate alpha share
+	    alpha_shares = []*n_parties
+	    for i in range(n_parties):
+                y_lam = wire.lambda_val(self.y)[i]
+                y_lamh = wire.lambda_hat(self.y)[i]
+                alpha_shares[i] = epsilon_1[i]*y_lam + (epsilon_2[i]*y_lamh)
+            alpha_broadcast.append(alpha_shares)	
+	if c.operation == 'ADD' or c.operation== 'XOR':
+		c.add()
+    return alpha_broadcast
 
-#input:array of circuit class
+#input:array of circuit class, wire object
 #output: NA
 #Assign e values of input wires
-def compute_e(circuit):
-	for i in range(n_gate):
-		c = circuit[i]
-		#calculate lambdas and vs from shares
-		lam_x = 0 
-		lam_y = 0
-		v_x = 0
-		v_y = 0
-		for j in range(n_parties):
-			lam_x = lam_x + wire.acceess_wire(c.x, lambda, j)
-			lam_y = lam_y + wire.acceess_wire(c.y, lambda, j) 
-			v_x = v_x + wire.acceess_wire(c.x, v, j)
-			v_y = v_y + wire.acceess_wire(c.y, v, j)
-		wire.insert_wire(c.x, 'e', lam_x + v_x)
-		wire.insert_wire(c.y, 'e', lam_y + v_y) 
+def compute_e(circuit, wire):
+    for i in range(n_gate):
+        c = circuit[i]
+        #calculate and set x_e
+        x_v = sum(wire.v(c.x).value)
+	x_lam = sum(wire.lambda_val(c.x).val)
+        x_e = Value(x_v + x_lam)
+	wire.set_e(c.x, x_e)
+	#calculate and set y_v
+        y_v = sum(wire.v(c.y).value)
+        y_lam = sum(wire.lambda_val(c.y).val)
+        y_e = Value(y_v + y_lam)
+        wire.set_e(c.y, y_e)
+    return 1
+'''
 
-def main():
-	#Parse data 
-	input_stream = sys.argv[1]
-	with open(input_stream) as f:
-		first_line = f.readline().split()
-		global n_gate
-		n_gate = int(first_line[0])
-		global n_wire
-		n_wire = int(first_line[1])
-		second_line = f.readline().split()
-		global n_input
-		n_input = int(second_line[0])
-		#create list of number of wires for input values
-		global l_input
-		l_input = [None]*n_input
-		for i in range(n_input):
-			l_input[i] = int(second_line[i+1])
-		third_line = f.readline().split()
-		global n_output
-		n_output = int(third_line[0])
-		#create list of number of wires for output values
-		global l_output
-		l_output = [None]*n_output
-		for i in range(n_output):
-			l_output[i]=int(third_line[i+1])
-		#create list of gate
-		global circuit
-		circuit = parse(f, n_gate)	
-	#Create list of wire data
-	global n_parties	
-	n_parties = 3
-	global wire_data
-	wire_data = [{'e' : None, 'v': []*n_parties, 'lambda': []*n_parties} for i in range(n_wire)]
-if __name__ == '__main__':
-     main()
+'''
 
