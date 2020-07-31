a507edc67d429b97ed1d55c38cfc1c2ce28ec591
zilyang <zilingyang@DESKTOP-8FVRHLF.localdomain>
2020-07-30 20:55:14 -0400

    commit

--- circuit.py
+++ circuit.py
@@ -52,46 +52,33 @@ def wire_data(n_wires):
 
 #output: array of e broadcast 
 #Write v shares and e to output value
-def compute_output(circuit, epsilon_1, epsilon_2, wire):
+def compute_output(circuit, epsilon_1, epsilon_2, wire, n_gate, n_parties):
     alpha_broadcast = []
     for i in range(n_gate):
         c = circuit[i]
-        if c.operation == 'MUL' or c.operaton == 'AND':
-            c.mul()
+        print('x:', c.x)
+        print('y:', c.y)
+        if c.operation == 'MUL' or c.operation == 'AND':
+            c.w = wire
+            c.mult()
+            print('z:', c.z)
 	    # calculate alpha share
-            alpha_shares = []*n_parties
-            for i in range(n_parties):
-                y_lam = wire.lambda_val(self.y)[i]
-                y_lamh = wire.lambda_hat(self.y)[i]
-                alpha_shares[i] = epsilon_1[i]*y_lam + (epsilon_2[i]*y_lamh)
+            alpha_shares = [None]*n_parties
+            for j in range(n_parties):
+                y_lam = wire.lambda_val(c.y)[i]
+                y_lamh = wire.lam_hat(c.y)[i]
+                alpha_shares[j] = epsilon_1[i]*y_lam + (epsilon_2[i]*y_lamh)
             alpha_broadcast.append(alpha_shares)	
         if c.operation == 'ADD' or c.operation== 'XOR':
+            c.w = wire
             c.add()
+            print('z:', c.z)
     return alpha_broadcast
-
-#input:array of circuit class, wire object
-#output: NA
-#Assign e values of input wires
-def compute_e(circuit, wire, n_gate):
-    for i in range(n_gate):
-        c = circuit[i]
-        #calculate and set x_e
-        x_v = sum(wire.v(c.x))
-        print('lambda:', wire.lambda_val(c.x))
-        x_lam = sum(wire.lambda_val(c.x))
-        x_e = (x_v + x_lam)
-        wire.set_e(c.x, x_e)
-	#calculate and set y_v
-        y_v = sum(wire.v(c.y))
-        y_lam = sum(wire.lambda_val(c.y))
-        y_e = (y_v + y_lam)
-        wire.set_e(c.y, y_e)
-    return 1
 
 #input: circuit, wire structure, list of n_mul gate alphas, and two epsilons
 #output: n_parties zeta shares
 def compute_zeta_share(circuit, wire, alpha, epsilon_1, epsilon_2, n_parties):
-    r = []*n_parties
+    r = [None]*n_parties
     
     for i in range(n_parties):
         zeta = 0
@@ -101,13 +88,14 @@ def compute_zeta_share(circuit, wire, alpha, epsilon_1, epsilon_2, n_parties):
                 x = circuit[j].x
                 y = circuit[j].y
                 z = circuit[j].z
-                A = alpha[i][n]
-                s = (epsilon_1 * wire.e(y) - A)* wire.lambda_val(x)[j] + \
-                    epsilon_1 * wire.e(x) * wire.lambda_val(y)[j] - \
-                    epsilon_2 * wire.e(z) - epsilon_2 * wire.lam_hat(z)     
+                A = alpha[n][i]
+                s = (epsilon_1[j] * wire.e(y) - A)* wire.lambda_val(x)[j] + \
+                    epsilon_1[j] * wire.e(x) * wire.lambda_val(y)[j] - \
+                    epsilon_2[j] * wire.e(z) - epsilon_2[j] * wire.lam_hat(z)[i]     
                 n += 1
-            if i == 0:
-                s += epsilon_1 * wire.e(z) - epsilon_1*wire.e(x)*wire.e(y) + epsilon_2*wire.e_hat(z)
-        zeta += s
-    r.append(zeta)
+                if i == 0:
+                    print('e_hat:', wire.e_hat(z))
+                    s += epsilon_1[j] * wire.e(z) - epsilon_1[j]*wire.e(x)*wire.e(y) + epsilon_2[j]*wire.e_hat(z)
+                zeta += s
+        r[i] = (zeta)
     return r 
