115f770363adc0536a09264f24eee35f0792cca7
zilyang <zilingyang@DESKTOP-8FVRHLF.localdomain>
2020-07-30 20:55:26 -0400

    commit

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import sys
 import random
 import circuit 
 from gate import gate
-from wire import Wire
+from gate import Wire 
 import Preprocessing as p
 from Value import Value
 
@@ -15,7 +15,7 @@ def test():
     n_output = Circuit[5]
     l_output = Circuit[2]
     Circuit = Circuit[0]
-    print(Circuit[0].x, Circuit[0].y, Circuit[0].z)
+   
     # Create list of wire data
     n_parties = 3    
     wire_data = circuit.wire_data(n_wires)
@@ -30,14 +30,34 @@ def test():
     for i in range(n_input):
         w.set_v(i, inputs[i].splitVal(n_parties))
     triples = p.assignLambda(Circuit, w, n_parties)  
-    alpha = circuit.compute_output
-    print('0:', w.v(0))
-    print('1:', w.v(1))
-    print('2:', w.v(2))
-    print('3:', w.v(3))
-    print('4:', w.v(4))
+    epsilon_1 = Value()
+    epsilon_2 = Value()
+    epsilon_1.getRand()
+    epsilon_2.getRand()
+    if n_gate == 1:
+        epsilon_1 = [epsilon_1]
+        epsilon_2 = [epsilon_2]
+    else:
+        epsilon_1 = epsilon_1.splitVal(n_gate)
+        epsilon_2 = epsilon_2.splitVal(n_gate)
+    alpha = circuit.compute_output(Circuit, epsilon_1, epsilon_2, w, n_gate, n_parties)
     
-    circuit.compute_e(Circuit, w, n_gate)     
-
+    for j in range(n_gate):
+        g = Circuit[j]
+        if g.operation == 'XOR' or g.operation =='ADD': 
+            assert(sum(w.v(g.z)) == (sum(w.v(g.x)) + sum(w.v(g.y))))
+            for i in range(n_parties):
+                assert(w.lambda_val(Circuit[j].x)[i] + w.lambda_val(Circuit[j].y)[i] == w.lambda_val(Circuit[j].z)[i])
+        if g.operation == 'AND' or g.operation == 'MUL':
+            for i in range(n_parties):
+                assert(g.a[i] == w.lambda_val(g.x)[i])
+                assert(g.b[i] == w.lam_hat(g.y)[i])
+                
+            assert(sum(g.a)*sum(g.b) == w.e_hat(g.z) - sum(w.lam_hat(g.z)))
+            assert(sum(w.v(g.z)) == (sum(w.v(g.x)) * sum(w.v(g.y)))) 
+    for i in range(n_wires):
+        assert(w.e(i) == sum(w.lambda_val(i)) + sum(w.v(i)))
+    zeta = circuit.compute_zeta_share(Circuit, w, alpha, epsilon_1, epsilon_2, n_parties)
+    assert(sum(zeta).value == 0)
 if __name__ == "__main__": 
     test() 
