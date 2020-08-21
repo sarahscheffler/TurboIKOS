17af09da87e82dec2e7a5e35bc4c87db03db77b4
Julie Ha <julieha124@gmail.com>
2020-08-21 02:39:03 -0400

    fiat shamir updates

--- Fiat-Shamir.py
+++ Fiat-Shamir.py
@@ -2,14 +2,14 @@
 Perform the Fiat-Shamir transformation with SHA256 for non-interactive proof 
 """
 
-from hashlib import sha256
-from gmpy2 import mpz, t_mod, sub
-from Crypto.Util.number import bytes_to_long
 from binascii import hexlify
-from random import sample, seed
+from hashlib import sha256
+from random import sample, seed, getrandbits
+
+from Crypto.Util.number import bytes_to_long
+from gmpy2 import mpz, sub, t_mod
 
 field = mpz(2**127-1) #field value 
-reject_value = sub(field, t_mod(field, 4)) #QUESTION: why are we breaking it into 4 equal parts? is there a reason? 
 
 def make_epsilons(r2, num_mult_gates):
     """
@@ -20,6 +20,40 @@ def make_epsilons(r2, num_mult_gates):
     outputs: 2*num_mult_gates epsilsons 
     """    
     list_epsilon = [0]*num_mult_gates
+
+    #VERSION 2 : generate 128 random bits by using random.getrandbits, splice value 
+    seed(r2)
+
+    for i in range(num_mult_gates):
+        epsilon = bin(getrandbits(128))[:127]
+        epsilon_hat = bin(getrandbits(128))[:127]
+        list_epsilon[i] = [epsilon, epsilon_hat]
+
+    """
+    ---DRAFT VERSION 1.1----
+    for i in range(num_mult_gates):
+        mid_value_1 = 0
+        mid_value_2 = mid_value_1+1 
+
+        temp_1 = bytes_to_long(sha256(str(i).encode() + str(mid_value_1).encode() + r2)) #hash(i || mid_value_1 || r2) --> coverted to long 
+        temp_2 = bytes_to_long(sha256(str(i).encode() + str(mid_value_2).encode() + r2)) #hash(i || mid_value_2 || r2) --> coverted to long 
+
+        if bin(temp_1)[:127] == '0b' + ('1'*125):
+            epsilon = bin(temp_1)[127:254]
+        else: 
+            epsilon = bin(temp_1)[:127]
+        
+        if bin(temp_2)[:127] == '0b' + ('1'*125):
+            epsilon = bin(temp_2)[127:254]
+        else: 
+            epsilon = bin(temp_2)[:127]
+
+        list_epsilon[i] = [epsilon, epsilon_hat]
+    """
+
+
+    """
+    ---DRAFT VERSION 1----
     for i in range(num_mult_gates):
         mid_value_1 = 0
         mid_value_2 = mid_value_1+1 
@@ -42,6 +76,7 @@ def make_epsilons(r2, num_mult_gates):
 
 
         list_epsilon[i] = [epsilon, epsilon_hat]
+    """
     return list_epsilon
 
 def round2(round_1, num_mult_gates):
@@ -87,7 +122,6 @@ def round4(round_1, round_3, t, n):
         return [x for x in range(n) if x != do_not_open]
     else:
         list_parties = [x for x in range(field)]
-        mid_value = 0 #TO DO: add rejection sample 
-        temp = bytes_to_long(sha(str(i).encode() + str(mid_value).encode() + r4))
-        seed(t_mod(mpz(temp), n)) #QUESTION i don't think this works? but wondering if something similar exists to set seed as value generated from hashed value 
+        hash_value = bytes_to_long(sha(str(i).encode() + str(mid_value).encode() + r4))
+        seed(hash_value) #QUESTION i don't think this works? but wondering if something similar exists to set seed as value generated from hashed value 
         return sample(list_parties, t)
