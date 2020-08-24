996397ad9a1dd28eacf8c3ec53ded6d2e625f838
zilyang <zilingyang@DESKTOP-8FVRHLF.localdomain>
2020-08-24 01:46:34 -0400

    add test prover.py

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ import sys
 import random
 import circuit 
 from gate import gate
-from gate import Wire 
+from wire import Wire 
 import Preprocessing as p
 from Value import Value
+import prover
 
 def test():
     Circuit = circuit.parse(gate)
@@ -14,12 +15,17 @@ def test():
     n_input = Circuit[6]
     n_output = Circuit[5]
     l_output = Circuit[2]
+    n_mul = Circuit[8]
     Circuit = Circuit[0]
    
     # Create list of wire data
     n_parties = 3    
     wire_data = circuit.wire_data(n_wires)
     w = Wire(wire_data, n_parties, n_wires)
+
+    #Preprocessing
+    triples = p.assignLambda(Circuit, w, n_parties)
+
     #Assign v values
     inputs = []
     for i in range(n_input):
@@ -29,18 +35,22 @@ def test():
        
     for i in range(n_input):
         w.set_v(i, inputs[i].splitVal(n_parties))
-    triples = p.assignLambda(Circuit, w, n_parties)  
-    #Generate epsilons
+    triples = p.assignLambda(Circuit, w, n_parties)
+    #Commit round one
+    views = prover.round_one(n_parties, n_gate, n_input, Circuit, w)
+    views_commit = views[0]
+    views = views[1]
+    #Generate epsilons TODO: replace with fiat shamir
     epsilon_1 = Value()
     epsilon_2 = Value()
     epsilon_1.getRand()
     epsilon_2.getRand()
-    if n_gate == 1:
+    if n_mul == 1:
         epsilon_1 = [epsilon_1]
         epsilon_2 = [epsilon_2]
     else:
-        epsilon_1 = epsilon_1.splitVal(n_gate)
-        epsilon_2 = epsilon_2.splitVal(n_gate)
+        epsilon_1 = epsilon_1.splitVal(n_mul)
+        epsilon_2 = epsilon_2.splitVal(n_mul)
 
     alpha = circuit.compute_output(Circuit, epsilon_1, epsilon_2, w, n_gate, n_parties)
     m = 0
@@ -48,7 +58,7 @@ def test():
         g = Circuit[i]
         if g.operation == 'AND' or g.operation == 'MUL':
             for j in range(n_parties):
-                assert(alpha[m][j] == epsilon_1[i]*w.lambda_val(g.y)[j] + epsilon_2[i]*w.lam_hat(g.y)[j])
+                assert(alpha[m][j] == epsilon_1[m]*w.lambda_val(g.y)[j] + epsilon_2[m]*w.lam_hat(g.y)[j])
             m = m + 1
         
     for j in range(n_gate):
@@ -78,6 +88,10 @@ def test():
     zeta = circuit.compute_zeta_share(Circuit, w, alpha, epsilon_1, epsilon_2, n_parties)
     #Check zeta
     assert(sum(zeta).value == 0)
+    #Commit to broadcast
+    broadcast = prover.round_three(n_parties, n_gate, n_input, Circuit, w, alpha, zeta)
+    broadcast_commit = broadcast[0]
+    broadcast = broadcast[1]
     print('test passed')
 if __name__ == "__main__": 
     test() 
