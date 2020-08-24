b27d6ba0875726739ceef352b1a2a05a231062be
zilyang <zilingyang@DESKTOP-8FVRHLF.localdomain>
2020-08-24 01:46:57 -0400

    debugged

--- prover.py
+++ prover.py
@@ -21,9 +21,9 @@ def open(r, v, commit):
     return commit == hashlib.sha256(long_to_bytes(r) + v.encode()).hexdigest()
 
 #inputs: number of parties, number of gates, circuit object, wire object
-#output: commited broad cast, broad cast, commit views, views
+#output: commit views, views
 #output first round    
-def round_one(n_parties, n_gate, circuit, wire):
+def round_one(n_parties, n_gate, n_input, circuit, wire):
     views_commit = [None]*n_parties
     views = [None]*n_parties
         
@@ -32,6 +32,7 @@ def round_one(n_parties, n_gate, circuit, wire):
     for j in range(n_parties):
         d = {'input': [], 'input lambda': [], 'lambda z': [], 'lambda y hat': [], 'lambda z hat': []}
         views_str = b''
+        
         for i in range(n_gate):
             g = circuit[i]
             input_str = b''
@@ -47,12 +48,12 @@ def round_one(n_parties, n_gate, circuit, wire):
             if g.y < n_input:
                 d['input'].append(wire.v(g.y)[j])
                 d['input lambda'].append(wire.lambda_val(g.y)[j])
-            if g.operation == 'MUL' or 'AND':
-                views[j]['lambda z'].append( wire.lambda_val(g.z)[j])
+            if g.operation == 'MUL' or g.operation == 'AND':
+                d['lambda z'].append( wire.lambda_val(g.z)[j])
                 lam_z_str += long_to_bytes(wire.lambda_val(g.z)[j].value)
-                views[j]['lambda y hat'].append(wire.lam_hat(g.y)[j])
+                d['lambda y hat'].append(wire.lam_hat(g.y)[j])
                 lam_y_hat_str += long_to_bytes(wire.lam_hat(g.y)[j].value)
-                views[j]['lambda z hat'].append(wire.lam_hat(g.z)[j])
+                d['lambda z hat'].append(wire.lam_hat(g.z)[j])
                 lam_z_hat_str += long_to_bytes(wire.lam_hat(g.z)[j].value)
         views[j] = d
         views_str = input_str + input_lam_str + lam_z_str + lam_y_hat_str + lam_z_hat_str
@@ -62,7 +63,7 @@ def round_one(n_parties, n_gate, circuit, wire):
 
 #input: number of gates, circuit object, wire object, alpha arry arr[#parties][#mulgates], zeta[#parties]
 #output: committed broadcast, broadcast
-def round_three(n_gate, circuit, wire, alpha, zeta):
+def round_three(n_parties, n_gate, n_input, circuit, wire, alpha, zeta):
     e_inputs = []
     e_input_str = b''
     e_z = []
@@ -72,11 +73,12 @@ def round_three(n_gate, circuit, wire, alpha, zeta):
     e_z_str = b''
     e_z_hat_str = b''
     alpha_str = b''
- 
+    zeta_str = b''
+
     # v output shares
     output_shares = wire.v(circuit[-1].z)
     for j in range(n_parties):
-        output_shares_str += long_to_bytes(wire.v(circuit[-1].z).value)
+        output_shares_str += long_to_bytes(wire.v(circuit[-1].z)[j].value)
         zeta_str += long_to_bytes(zeta[j].value)
 
     n_mul = 0
@@ -86,7 +88,7 @@ def round_three(n_gate, circuit, wire, alpha, zeta):
             e = wire.e(i)
             e_input_str += long_to_bytes(e.value)
             e_inputs.append(e)
-        if circuit[i].operation == 'MUL' or 'AND':
+        if circuit[i].operation == 'MUL' or circuit[i].operation == 'AND':
             g = circuit[i]
             #e_z
             val = wire.e(g.z)
@@ -94,11 +96,12 @@ def round_three(n_gate, circuit, wire, alpha, zeta):
             e_z.append(val)
             #ez hat
             val = wire.e_hat(g.z)
-            e_z_hat_str += long_to_bytes(val.value)e_z_hat.append(val)
+            e_z_hat_str += long_to_bytes(val.value)
             e_z_hat.append(val)
             #alpha
             for j in range(n_parties):
-                alpha_str += long_to_bytes(alpha[j][n_mul].value)   
+                alpha_str += long_to_bytes(alpha[n_mul][j].value)   
+            n_mul += 1
 
     broadcast = {'e inputs': e_inputs, 'e z': e_z, 'e z hat': e_z_hat, 'alpha': alpha, 'zeta': zeta, 'output shares': output_shares}
     broadcast_str = e_input_str + e_z_str + e_z_hat_str + alpha_str + zeta_str + output_shares_str
